f03ecfb0d5ddf18339b116c416ed5bdf5cdde69f
mdsohag65 <sohag92ahmed@gmail.com>
2022-08-25 19:56:50 +0600

    header section done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Header.css';
+import bg from '../../images/background.png';
+import logo from '../../images/logo.png';
+
+const Header = () => {
+    return (
+        <section style={{
+            background: `url(${bg})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+        }} className='header'>
+            <div className="container">
+                <nav>
+                    <img src={logo} alt="" className='logo' />
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Portfolio</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+                <div className="header-text">
+                    <p><span>React</span> FrontEnd Developer</p>
+                    <h1>Hi, I'm <span>Kevin</span><br /> XI From Australia</h1>
+                </div>
+            </div>
+
+        </section>
+    );
+};
+
+export default Header;
